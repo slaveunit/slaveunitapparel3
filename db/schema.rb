9b439904de585496bb40e80ea5e58a01cbf3789c
slaveunit <marco.abundo@live.com>
2016-04-28 00:22:48 +0000

    g model order_status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428000725) do
+ActiveRecord::Schema.define(version: 20160428002136) do
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
