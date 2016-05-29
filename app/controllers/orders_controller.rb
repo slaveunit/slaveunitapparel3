d3b4c440053f92e8b983a8aa560bc46e04d33572
slaveunit <marco.abundo@live.com>
2016-05-29 17:30:30 +0000

    Size of product appears on shopping cart, size params saved as order_item.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,7 +35,7 @@ class OrdersController < ApplicationController
 	private
 
 	def order_params
-		params.require(:order).permit(:product_id, :user_id, :total)
+		params.require(:order).permit(:product_id, :product_size, :user_id, :total)
 	end
 
 end
